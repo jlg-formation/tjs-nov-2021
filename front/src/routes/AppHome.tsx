dc4f48e77160a84b36e2b4c668efb20ae5c9bef9
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-30 14:38:34 +0100

    dev: added dom and css of home

--- front/src/routes/AppHome.tsx
+++ front/src/routes/AppHome.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function AppHome() {
-  return <div>AppHome ok...</div>;
+  return (
+    <main>
+      <h1>Gérer votre stock efficacement.</h1>
+      <Link to="/stock">
+        <button className="primary">Voir le stock</button>
+      </Link>
+    </main>
+  );
 }
 
 export default AppHome;
