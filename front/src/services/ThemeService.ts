c5673b2e9b04c8feb5eed4d22eb6246b1fc13d9f
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-30 11:34:28 +0100

    dev: added dark light theme
    used CSS custom properties

--- front/src/services/ThemeService.ts
+++ front/src/services/ThemeService.ts
@@ -5,6 +5,15 @@ export type ThemeValue = "dark" | "light";
 class ThemeService {
   theme$ = new BehaviorSubject<ThemeValue>("light");
 
+  constructor() {
+    this.theme$.subscribe((theme) => {
+      ["dark", "light"].forEach((theme) => {
+        document.body.classList.remove(theme);
+      });
+      document.body.classList.add(theme);
+    });
+  }
+
   toggle() {
     if (this.theme$.value === "light") {
       this.theme$.next("dark");
