3a5dd638e049d46d93ea626ef84dba92b23e2d91
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-30 16:56:40 +0100

    dev: sync and async

---
+++ examples/async/promise.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+const readdir = (dirname) => {
+  return new Promise((resolve, reject) => {
+    readdir(dirname);
+  });
+};
--- examples/async/sync.js
+++ examples/async/sync.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 
 try {
-  const files = fs.readdirSync(".");
+  const files = fs.readdir(".");
   console.log("files: ", files);
+  const content = fs.readFile(files[0], { encoding: "utf-8" });
+  console.log("content: ", content);
 } catch (err) {
   console.log("err: ", err);
 }
