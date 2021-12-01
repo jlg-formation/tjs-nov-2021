ffc4e18f84076caae24477900ce83d4445a72a81
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-01 15:31:18 +0100

    dev: added offline mode with localstorage

--- front/src/layout/AppFooter.tsx
+++ front/src/layout/AppFooter.tsx
@@ -1,9 +1,22 @@
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { ArticleContext } from "../contexts/ArticleContext";
 
 function AppFooter() {
+  const articleService = useContext(ArticleContext);
+  const [serverStatus, setServerStatus] = useState(true);
+
+  useEffect(() => {
+    articleService.isServerReachable$.subscribe({
+      next: (s) => {
+        setServerStatus(s);
+      },
+    });
+  }, [articleService]);
   return (
     <footer>
       <Link to="/legal">Mentions Légales</Link>
+      <div>Server Status : {serverStatus ? "on" : "off"}</div>
     </footer>
   );
 }
--- front/src/services/ArticleService.ts
+++ front/src/services/ArticleService.ts
@@ -1,11 +1,23 @@
-import { map, switchMap, timeout } from "rxjs/operators";
+import {
+  catchError,
+  distinctUntilChanged,
+  map,
+  switchMap,
+  tap,
+  timeout,
+} from "rxjs/operators";
 import { fromFetch } from "rxjs/fetch";
 import { Article } from "../interfaces/Article";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, of } from "rxjs";
 
 const url = "/api/articles";
 
 class ArticleService {
+  private isServerReachablePrivate$ = new BehaviorSubject(true);
+  isServerReachable$ = this.isServerReachablePrivate$.pipe(
+    distinctUntilChanged()
+  );
+
   add(newArticle: {
     name: string;
     price: number;
@@ -33,7 +45,24 @@ class ArticleService {
     return fromFetch(url).pipe(
       timeout(5000),
       switchMap((response) => {
+        if (response.status >= 400) {
+          throw new Error("error from server");
+        }
         return response.json() as Promise<Article[]>;
+      }),
+      tap((articles) => {
+        localStorage.setItem("articles", JSON.stringify(articles));
+        this.isServerReachablePrivate$.next(true);
+      }),
+      catchError((err) => {
+        console.log("err: ", err);
+        this.isServerReachablePrivate$.next(false);
+        const str = localStorage.getItem("articles");
+        if (!str) {
+          throw new Error("no localstorage");
+        }
+        const articles = JSON.parse(str) as Article[];
+        return of(articles);
       })
     );
   }
