e73b2e80096b8d9a5f51dc5a47a48d59e45d9401
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-01 09:42:47 +0100

    dev: added async await and promise

--- examples/async/promise.js
+++ examples/async/promise.js
@@ -2,6 +2,36 @@ const fs = require("fs");
 
 const readdir = (dirname) => {
   return new Promise((resolve, reject) => {
-    readdir(dirname);
+    fs.readdir(dirname, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result);
+    });
   });
 };
+
+const readFile = (filename, options) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, options, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result);
+    });
+  });
+};
+
+readdir(".")
+  .then((files) => {
+    console.log("files: ", files);
+    return readFile(files[0], { encoding: "utf-8" });
+  })
+  .then((content) => {
+    console.log("content: ", content);
+  })
+  .catch((err) => {
+    console.log("err: ", err);
+  });
